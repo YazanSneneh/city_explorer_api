c53aa361ba95ae18c3f7310fee0215b0ec07a15e
yazan <yaz.sneneh@gmail.com>
2021-02-23 00:44:52 +0200

    start park route

--- server.js
+++ server.js
@@ -12,12 +12,11 @@ const PORT = process.env.PORT;
 // ............................................................................ routes
 app.get('/location', handleLocation);
 app.get('/weather', handleWeather)
+app.get('/parks', handleParks)
 app.get('*', handle404)
-
 //............................................................ handle requests functions
 function handleLocation(req, res) {
     const query = req.query.city;
-    console.log(query)
     locationLonLat(query, res);
 }
 // ........................weather data request
@@ -42,6 +41,20 @@ function handleWeather(req, res) {
 function handle404(req, res) {
     res.status(404).send('<h1> INVALID URL, PAGE NOT FOUND 404</h1>')
 }
+
+// handleParks
+function handleParks(req, res) {
+    let parkKeys = {
+        key: process.env.PARKS_API_KEY
+    }
+    superagent.get(`https://developer.nps.gov/api/v1/parks?parkCode=acad&)
+        .then(data => {
+            console.log(data)
+            res.status(200).send(data)
+        }).catch(error => {
+            res.status(500).send(error)
+        })
+}
 //........................................................................... functions
 // handle location data request 
 const locationLonLat = (query, res) => {
@@ -54,17 +67,17 @@ const locationLonLat = (query, res) => {
     try {
         superagent.get(`https://eu1.locationiq.com/v1/search.php?`).query(resKeys)
             .then(data => {
-                const lon = data.body[0].lon;
-                const lat = data.body[0].lat;
-                const display = data.body[0].display_name;
-                const resObj = new CityObject(query, display, lon, lat);
-                return res.status(200).send(resObj);
-            }).catch(err => {
-                console.log(err)
-            })
+        const lon = data.body[0].lon;
+        const lat = data.body[0].lat;
+        const display = data.body[0].display_name;
+        const resObj = new CityObject(query, display, lon, lat);
+        return res.status(200).send(resObj);
+    }).catch(err => {
+        console.log(err)
+    })
     } catch (error) {
-        return res.status(500).send('error occured please try again later : ' + error);
-    }
+    return res.status(500).send('error occured please try again later : ' + error);
+}
 }
 // convert string format
 function formateDate(time) {
