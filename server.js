0611f203b807deacf9381035f520ed997605fbc1
yazan <yaz.sneneh@gmail.com>
2021-02-23 14:49:17 +0200

    refactor

--- server.js
+++ server.js
@@ -3,31 +3,65 @@
 const express = require('express');
 const cors = require('cors');
 const superagent = require('superagent');
-require('dotenv').config();
 
+
+// use packages
 const app = express();
 app.use(cors());
-const PORT = process.env.PORT;
+require('dotenv').config();
 
+
+// ..................................................................... app variables
+const PORT = process.env.PORT;
 // ............................................................................ routes
 app.get('/location', handleLocation);
 app.get('/weather', handleWeather)
 app.get('/parks', handleParks)
 app.get('*', handle404)
+
 //............................................................ handle requests functions
+// ........... handle Location data request
 function handleLocation(req, res) {
     const query = req.query.city;
-    locationLonLat(query, res);
+    let url = `https://eu1.locationiq.com/v1/search.php?`
+    const resKeys = {
+        key: process.env.GEOCODE_API_KEY,
+        q: query,
+        format: 'json',
+        limit: 1
+    }
+
+    try {
+        superagent.get(url).query(resKeys)
+            .then(data => {
+                // data received
+                const lon = data.body[0].lon;
+                const lat = data.body[0].lat;
+                const display = data.body[0].display_name;
+
+                const resObj = new CityObject(query, display, lon, lat);
+
+                return res.status(200).send(resObj);
+
+            }).catch(error => {
+                console.log('error eccourred while requesting data : ' + error)
+            })
+    } catch (error) {
+        return res.status(500).send('error occured please try again later : ' + error);
+    }
 }
+
 // ........................weather data request
 function handleWeather(req, res) {
+    const url = `http://api.weatherbit.io/v2.0/forecast/daily`;
     const weatherQuery = {
         key: process.env.WEATHER_API_KEY,
         lon: req.query.lon,
-        lat: req.query.lat
+        lat: req.query.lat,
+        days: 4
     }
 
-    superagent.get(`http://api.weatherbit.io/v2.0/forecast/daily`).query(weatherQuery)
+    superagent.get(url).query(weatherQuery)
         .then(response => {
             let weatherObjects = response.body.data.map(day => {
                 var dayInfo = new Weather(day.weather.description, formateDate(day.datetime))
@@ -38,16 +72,17 @@ function handleWeather(req, res) {
             res.status(500).send(err)
         })
 }
+// .................... handle 404 page
 function handle404(req, res) {
     res.status(404).send('<h1> INVALID URL, PAGE NOT FOUND 404</h1>')
 }
 
-// handleParks
+// ......................handleParks
 function handleParks(req, res) {
     let parkKeys = {
         key: process.env.PARKS_API_KEY
     }
-    superagent.get(`https://developer.nps.gov/api/v1/parks?parkCode=acad&)
+    superagent.get(`https://developer.nps.gov/api/v1/parks?parkCode=acad&`)
         .then(data => {
             console.log(data)
             res.status(200).send(data)
@@ -56,29 +91,6 @@ function handleParks(req, res) {
         })
 }
 //........................................................................... functions
-// handle location data request 
-const locationLonLat = (query, res) => {
-    const resKeys = {
-        key: process.env.GEOCODE_API_KEY,
-        q: query,
-        format: 'json',
-        limit: 5
-    }
-    try {
-        superagent.get(`https://eu1.locationiq.com/v1/search.php?`).query(resKeys)
-            .then(data => {
-        const lon = data.body[0].lon;
-        const lat = data.body[0].lat;
-        const display = data.body[0].display_name;
-        const resObj = new CityObject(query, display, lon, lat);
-        return res.status(200).send(resObj);
-    }).catch(err => {
-        console.log(err)
-    })
-    } catch (error) {
-    return res.status(500).send('error occured please try again later : ' + error);
-}
-}
 // convert string format
 function formateDate(time) {
     let date = new Date(time)
@@ -97,9 +109,8 @@ function formateDate(time) {
 function CityObject(query, display, lon, lat) {
     this.search_query = query;
     this.formatted_query = display;
-    this.longitude = lat;
-    this.latitude = lon;
-
+    this.latitude = lat;
+    this.longitude = lon;
 }
 
 function Weather(forecast, time) {
